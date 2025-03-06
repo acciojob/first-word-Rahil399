f9d9c34ccb2566e0d2438be54fea38f81818f117
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-03-06 15:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	let index = s.indexOf(" ");
+	return index === -1 ? s : s.substring (0, index) ;
 }
 
-// Do not change the code below
-
+//Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
